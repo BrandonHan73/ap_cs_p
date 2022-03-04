40e9fcb8dfa44bcf4338f456dad053817e4a3da7
Brandon Han <brandontthan2005@gmail.com>
2022-03-03 16:09:27 -0800

    Added submatrix and determinant

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        double[][] val = {{4, 3, 2, 2}, {0, 1, -3, 3}, {0, -1, 3, 3}, {0, 3, 1, 1}};
+        Matrix m = new Matrix(4, 4);
+        for(int r = 0; r < 4; r++) {
+            for(int c = 0; c < 4; c++) {
+                m.set(r, c, val[r][c]);
+            }
+        }
+
+        System.out.println(m);
+        System.out.println(m.determinant());
+
     }
 
 }
--- src/main/Matrix.java
+++ src/main/Matrix.java
@@ -28,6 +28,41 @@ public class Matrix {
         }
     }
 
+    @SuppressWarnings("SpellCheckingInspection")
+    public Matrix submatrix(int row, int column) {
+        assert(0 < rows && 0 < columns && row < rows && column < columns);
+        Matrix temp = new Matrix(rows - 1, columns - 1);
+        for(int r = 0; r < row; r++) {
+            for(int c = 0; c < column; c++) {
+                temp.set(r, c, get(r, c));
+            }
+            for(int c = column + 1; c < columns; c++) {
+                temp.set(r, c - 1, get(r, c));
+            }
+        }
+        for(int r = row + 1; r < rows; r++) {
+            for(int c = 0; c < column; c++) {
+                temp.set(r - 1, c, get(r, c));
+            }
+            for(int c = column + 1; c < columns; c++) {
+                temp.set(r - 1, c - 1, get(r, c));
+            }
+        }
+        return temp;
+    }
+
+    public double determinant() {
+        assert(rows == columns);
+        if(rows == 2) {
+            return (get(0, 0) * get(1, 1)) - (get(1, 0) * get(0, 1));
+        }
+        double temp = 0;
+        for(int c = 0; c < columns; c++) {
+            temp += Math.pow(-1, c) * get(0, c) * submatrix(0, c).determinant();
+        }
+        return temp;
+    }
+
     public Matrix cleanse(double value) {
         for(int r = 0; r < rows; r++) {
             for(int c = 0; c < columns; c++) {
@@ -35,6 +70,19 @@ public class Matrix {
             }
         }
         return this;
+    }
+
+    public String toString() {
+        StringBuilder temp = new StringBuilder("[");
+        for(int r = 0; r < rows; r++) {
+            temp.append("(");
+            for(int c = 0; c < columns; c++) {
+                temp.append(get(r, c)).append(" ");
+            }
+            temp.deleteCharAt(temp.length() - 1).append(") ");
+        }
+        temp.deleteCharAt(temp.length() - 1).append("]");
+        return temp.toString();
     }
 
 }
